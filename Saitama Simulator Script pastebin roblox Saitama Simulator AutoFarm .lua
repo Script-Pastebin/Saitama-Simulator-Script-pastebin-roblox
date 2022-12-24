39a6cb73c4901b9049ef45892f1f634640a0f49c
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:16 +0100

    commit zero

---
+++ Saitama Simulator Script pastebin roblox Saitama Simulator AutoFarm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/T58zcTj9"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							This very OP script u can farm speed and power SO fast
+						
+]]
